eef6b5dd2c9a88ee62c0c6a645e6af80ff499233
Patrik Ragnarsson <patrik@starkast.net>
2010-08-25 01:34:30 +0200

    Use Vimeo albums for videos

--- vimeo.rb
+++ vimeo.rb
@@ -3,9 +3,15 @@ require 'helpers.rb'
 require 'httparty'
 
 # http://vimeo.com/api/oembed.json?url=http%3A//vimeo.com/13657050
+# http://vimeo.com/api/v2/album/781338/videos.json
 
 def get_videos(event)
-  video_ids = get_attr("vimeo", event)
+  album_id = get_attr("vimeo_album", event)
+  video_ids = []
+  videos = HTTParty.get("http://vimeo.com/api/v2/album/#{album_id}/videos.json")
+  videos.each do |video|
+    video_ids << video["id"]
+  end
   list = []
   video_ids.each do |video_id|
     list << HTTParty.get("http://vimeo.com/api/oembed.json?url=http://vimeo.com/#{video_id}").parsed_response
